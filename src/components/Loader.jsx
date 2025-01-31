e1d7392591d265c9cb31881e1887afbb943b65b8
Adrian Netto <adrian.netto73@gmail.com>
2025-01-31 14:19:03 -0300

    feat: add clerk

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Loader() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="animate-spin rounded-full border-t-4 border-purple-400 border-solid h-10 w-10">
+      </div>
+    </div>
+  )
+}
